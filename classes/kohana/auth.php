99873d65c7aa0f298c7e7261a6286da04920caee
Jim Keener <jimktrains@gmail.com>
2010-10-18 12:58:07 -0400

    config changes hence other changes

--- classes/kohana/auth.php
+++ classes/kohana/auth.php
@@ -68,6 +68,9 @@ abstract class Kohana_Auth {
 		// Save the config in the object
 		$this->_config = $config;
 
+		if(array_key_exists('session_profile', $this->_config)){
+			$this->_config['session_profile'] = 'default';
+		}
 		$this->_session = Session::instance($this->_config['session_profile']);
 
 		$this->_enc = Encrypt::instance($this->_config['encrypt_profile']);		
